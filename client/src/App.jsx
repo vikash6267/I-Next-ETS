24390091bea767fe78588e39a998af5693f26284
Rishi <rishimaheshwari040@gmail.com>
2024-06-05 11:53:42 +0530

    bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,16 +26,13 @@ import WebAppDevelopment from "./components/development/WebAppDevelopment";
 import { admin } from "./context/admin";
 import AdminHome from "./admin/Home/Home";
 import Login from "./admin/auth/Login";
-import Header from "./admin/components/Header"
+import Header from "./admin/components/Header";
 
-import WebDevelopment from "./components/development/WebDevelopment";
-import WebAppDevelopment from "./components/development/WebAppDevelopment";
-
- 
 function App() {
   const [toggle, setToggle] = useState(false);
   const [loading, setLoading] = useState(true);
   const { adminData } = useContext(admin);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
@@ -45,12 +42,11 @@ function App() {
   }, []);
 
   return (
-    <div className="flex min-h-screen w-screen flex-col  ">
+    <div className="flex min-h-screen w-screen flex-col">
       {loading ? (
         <Loader />
       ) : (
         <>
-
           {!adminData ? (
             <div>
               <Navbar />
@@ -62,38 +58,19 @@ function App() {
                 <Route path="/workinfo" element={<HowWeWork />} />
                 <Route path="/portfolio" element={<PortFolio />} />
                 <Route path="/career" element={<Career />} />
-
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/prework" element={<WorkDone />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/workinfo" element={<HowWeWork />} />
-            <Route path="/portfolio" element={<PortFolio />} />
-            <Route path="/career" element={<Career />} />
-            {/* services  */}
-            {/* design  */}
-            <Route path="/webdesign" element={<WebDesign />} />
-            <Route path="/uiuxdesign" element={<UIUX />} />
-            <Route path="/productdesign" element={<Products />} />
-            <Route path="/brandidentity" element={<Brands />} />
-            <Route path="/mobile" element={<MobileUIUX />} />
-
-            {/* development  */}
-            <Route path="/webDevelopment" element={<WebDevelopment />} />
-            <Route path="/webAppDevelopment" element={<WebAppDevelopment />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-
-
-                {/* services  */}
+                {/* Services */}
+                {/* Design */}
                 <Route path="/webdesign" element={<WebDesign />} />
                 <Route path="/uiuxdesign" element={<UIUX />} />
                 <Route path="/productdesign" element={<Products />} />
                 <Route path="/brandidentity" element={<Brands />} />
                 <Route path="/mobile" element={<MobileUIUX />} />
+                {/* Development */}
+                <Route path="/webDevelopment" element={<WebDevelopment />} />
+                <Route
+                  path="/webAppDevelopment"
+                  element={<WebAppDevelopment />}
+                />
                 <Route path="*" element={<NotFound />} />
                 <Route path="/login" element={<Login />} />
               </Routes>
@@ -104,7 +81,7 @@ function App() {
             </div>
           ) : (
             <div>
-              <Header/>
+              <Header />
               <Routes>
                 <Route path="/" element={<AdminHome />} />
               </Routes>
